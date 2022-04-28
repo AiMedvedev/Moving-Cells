38df1eb7a4e24afd5a5a516634673d658899940f
Alex <forcablaugrana@gmail.com>
2022-04-29 02:13:59 +0300

    localStorage problem

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,4 @@
+
 const container = document.querySelector('.container');
 const squareBody = document.querySelector('.square-body');
 const blocks = document.querySelectorAll('.block');
@@ -7,30 +8,42 @@ const arrowRight = document.querySelectorAll('.arrow.right');
 const arrowTop = document.querySelectorAll('.arrow.top');
 const arrowBottom = document.querySelectorAll('.arrow.bottom');
 
-const playingField = /* JSON.parse(localStorage.getItem("gameStatus")) || */ [];
-blocks.forEach(item => playingField.push(item))
+let playingField = /* JSON.parse(localStorage.getItem('gameStatus')) || */ [];
 
+blocks.forEach(item => {
+    playingField.push(item);
+});
 
-/* const render = () => {
+localStorage.setItem('gameStatus', JSON.stringify(playingField));
+//JSON.parse(JSON.stringify(playingField));
+/* console.log(playingField);
+console.log(firstArr); */
+
+//console.log(playingField);
+//localStorage.setItem("gameStatus", JSON.stringify(playingField));
+
+const render = () => {
     squareBody.innerHTML = '';
-    playingField.forEach((item, i) => {
+    playingField = JSON.parse(localStorage.getItem('gameStatus'));
+    playingField.forEach((item) => {
         const div = document.createElement('div');
-    
+        
         div.classList.add('block');
-        div.innerHTML = `<div class="block-number">${item[i]}</div>
-        <div class="block-btn">
-            <div class="arrow left"><img src="img/arrow-left.svg" alt=""></div>
-            <div class="arrow right"><img src="img/arrow-right.svg" alt=""></div>
-            <div class="arrow top"><img src="img/arrow-up.svg" alt=""></div>
-            <div class="arrow bottom"><img src="img/arrow-down.svg" alt=""></div>
-        </div>`;
-
+        div.innerHTML = '<div class="block-number">' + item.innerText + '</div>' + 
+        '<div class="block-btn">' + 
+            '<div class="arrow left"><img src="img/arrow-left.svg" alt=""></div>' + 
+            '<div class="arrow right"><img src="img/arrow-right.svg" alt=""></div>' + 
+            '<div class="arrow top"><img src="img/arrow-up.svg" alt=""></div>' + 
+            '<div class="arrow bottom"><img src="img/arrow-down.svg" alt=""></div>' + 
+        '</div>';
+        
         squareBody.append(div);
         
-        //localStorage.setItem('gameStatus', JSON.stringify(playingField));
-        
     });
-}; */
+
+    localStorage.setItem('gameStatus', JSON.stringify(playingField));
+    playingField = JSON.parse(localStorage.getItem('gameStatus'));
+};
 
 
 //let newArr = playingField.map((_, i, a) => a.slice(i * 5, i * 5 + 5)).filter((el) => el.length);  для создания двумерного массива из обычного
@@ -48,44 +61,55 @@ container.addEventListener('click', (e) => {
         })
     }
 
-    if (e.target.matches('.arrow.left img')) {   
+    if (e.target.matches('.arrow.left img')) {
         getTargetIndex();
-
+        console.log(targetIndex);
         playingField[targetIndex] = [playingField[targetIndex - 1], playingField[targetIndex - 1] = playingField[targetIndex]][0];
 
-        const tmp = blocks[targetIndex].innerHTML;
+        localStorage.setItem('gameStatus', JSON.stringify(playingField));
+        render();
+        //playingField = JSON.parse(localStorage.getItem('gameStatus'));
+        /* const tmp = blocks[targetIndex].innerHTML;
         blocks[targetIndex].innerHTML = blocks[targetIndex - 1].innerHTML;
-        blocks[targetIndex - 1].innerHTML = tmp;
-    } 
+        blocks[targetIndex - 1].innerHTML = tmp; */
+    }
 
-    if (e.target.matches('.arrow.right img')) {   
+    if (e.target.matches('.arrow.right img')) {
         getTargetIndex();
-
+        console.log(targetIndex);
         playingField[targetIndex] = [playingField[targetIndex + 1], playingField[targetIndex + 1] = playingField[targetIndex]][0];
 
-        const tmp = blocks[targetIndex].innerHTML;
+        localStorage.setItem('gameStatus', JSON.stringify(playingField));
+        render();
+        //render();
+        /* const tmp = blocks[targetIndex].innerHTML;
         blocks[targetIndex].innerHTML = blocks[targetIndex + 1].innerHTML;
-        blocks[targetIndex + 1].innerHTML = tmp;
-    } 
+        blocks[targetIndex + 1].innerHTML = tmp; */
+    }
 
-    if (e.target.matches('.arrow.top img')) {   
+    if (e.target.matches('.arrow.top img')) {
         getTargetIndex();
-
+        console.log(targetIndex);
         playingField[targetIndex] = [playingField[targetIndex - 5], playingField[targetIndex - 5] = playingField[targetIndex]][0];
 
-        const tmp = blocks[targetIndex].innerHTML;
+        localStorage.setItem('gameStatus', JSON.stringify(playingField));
+        render();
+        //render();
+        /* const tmp = blocks[targetIndex].innerHTML;
         blocks[targetIndex].innerHTML = blocks[targetIndex - 5].innerHTML;
-        blocks[targetIndex - 5].innerHTML = tmp;
-    } 
+        blocks[targetIndex - 5].innerHTML = tmp; */
+    }
 
-    if (e.target.matches('.arrow.bottom img')) {   
+    if (e.target.matches('.arrow.bottom img')) {
         getTargetIndex();
 
         playingField[targetIndex] = [playingField[targetIndex + 5], playingField[targetIndex + 5] = playingField[targetIndex]][0];
-
-        const tmp = blocks[targetIndex].innerHTML;
+        console.log(targetIndex);
+        localStorage.setItem('gameStatus', JSON.stringify(playingField));
+        render();
+        //render();
+        /* const tmp = blocks[targetIndex].innerHTML;
         blocks[targetIndex].innerHTML = blocks[targetIndex + 5].innerHTML;
-        blocks[targetIndex + 5].innerHTML = tmp;
-    } 
+        blocks[targetIndex + 5].innerHTML = tmp; */
+    }
 })
-
